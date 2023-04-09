5ff41f27eb87e6788de3b6cf7e463e61a864e1e7
Frehiwot Haile <54357776+she-code@users.noreply.github.com>
2023-04-09 12:43:51 +0530

    created components and callback functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
-import React from "react";
-import Form from './Components/Form';
+import React, { useState } from "react";
+import Form from "./Components/Form";
+import AppContainer from "./Components/AppContainer";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+
 function App() {
+  const [state, setState] = useState("Home");
+  const closeForm = () => setState("Home");
+  const openForm = () => setState("Form");
   return (
-   <Form/>
+    <AppContainer>
+      <div className=" p-4 mx-auto bg-white shadow-lg rounded-xl w-1/2 px-5">
+        <Header title="Welcome to lesson 5 of react-typescript with #tailwindcss" />
+        {state === "Home" ? (
+          <Home openFormCB={openForm} />
+        ) : (
+          <Form  closeFormCB={closeForm} />
+        )}
+      </div>
+    </AppContainer>
   );
 }
 
---
+++ src/Components/Home.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import logo from "../logo.svg";
+export default function Home(props: { openFormCB: () => void }) {
+  return (
+    <div className="flex flex-col justify-center">
+      <div className="flex">
+        <img src={logo} alt="logo" className="h-48" />
+        <div className="item-center flex-1 justify-center h-48 flex">
+          <p className=" ">Welcome to home Page</p>
+        </div>
+      </div>
+      <button className="p-2 bg-blue-500 text-white font-bold" onClick={props.openFormCB}>
+        Open Form{" "}
+      </button>
+    </div>
+  );
+}

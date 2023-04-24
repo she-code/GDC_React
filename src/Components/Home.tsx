624748812994a492f459ca2e58a67a280fdc1d75
Frehiwot Haile <54357776+she-code@users.noreply.github.com>
2023-04-24 14:22:38 +0530

    Revert "Level four"

--- src/Components/Home.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import FormsList from "./FormsList";
-export default function Home() {
-  return (
-    <div className="flex flex-col justify-center  min-h-max  mb-8">
-      <FormsList />
-    </div>
-  );
-}
